4db8598e3c24608b3d365f96121d141eeb4ef8ff
Martin Holmer <martin.holmer@gmail.com>
2019-03-03 18:24:50 -0500

    Revise conda.recipe/remove_local_package.bat file

--- conda.recipe/remove_local_package.bat
+++ conda.recipe/remove_local_package.bat
@@ -1,7 +1,8 @@
 @echo off
-:: USAGE: ./remove_local_package.bat
-:: ACTION: (1) uninstall installed biztax package (conda uninstall)
+:: USAGE: > cd conda.recipe
+::        > remove_local_package
+:: ACTION: (1) uninstall local biztax package (conda uninstall)
 :: NOTE: for those with experience working with compiled languages,
 ::       removing a local conda package is analogous to a "make clean" operation
-conda uninstall biztax --yes
+conda uninstall --yes -c local -n base biztax
 exit 0
